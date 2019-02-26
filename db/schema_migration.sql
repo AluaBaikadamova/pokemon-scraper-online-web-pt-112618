a73110206b0b3e2468643510a882372b99433252
Alua Baikadamova <abaikad00@gmail.com>
2019-02-26 02:38:34 +0000

    Automatically backed up by Learn

--- db/schema_migration.sql
+++ db/schema_migration.sql
@@ -1,1 +1,3 @@
 CREATE TABLE IF NOT EXISTS pokemon(id INTEGER PRIMARY KEY, name TEXT, type TEXT);
+alter table pokemon add column hp INTEGER
+update pokemon set hp=60
--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,13 @@
 class Pokemon
+
+attr_accessor :id, :name, :type, :db
+
+def save
+
+end
+
+def find
+
+end
+
 end
